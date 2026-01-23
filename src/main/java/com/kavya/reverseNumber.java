c252a759e2a842f2b21c2dda9f2dfb57795b2d7e
kavyaa07 <kavyakumar.cr7@gmail.com>
2026-01-23 20:21:14 +0530

    My first commit

---
+++ src/main/java/com/kavya/reverseNumber.java
@@ -0,0 +1,21 @@
+package com.kavya;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int reverse = 0;
+        while(num!= 0){
+            int num2 = num % 10;
+            reverse = reverse * 10 + num2;
+            num = num/10;
+        }
+        System.out.println("Reversed number: "+ reverse);
+        sc.close();
+
+    }
+}
