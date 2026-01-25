618ce4c6904823afe984ae04a19097d103c93af0
Saurabh Kaushal <saurabhkaushal4040@gmail.com>
2026-01-25 20:12:21 +0530

    myfirstcommit

--- src/main/java/com/trishivax/saurabhgupta/Test.java
+++ src/main/java/com/trishivax/saurabhgupta/Test.java
@@ -2,6 +2,9 @@ package com.trishivax.saurabhgupta;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hey");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of two number is:"+ sum);
     }
 }
